7f1318e36ffbb3cb5ae17464fac594e05db7c8ac
Morten812 <150682994+Morten812@users.noreply.github.com>
2026-04-16 20:28:34 +0200

    Mail tilføjet

---
+++ Evento-Front-end/Evento-Front-end/DTOs/ServiceDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Evento_Front_end.DTOs
+{
+    public class ServiceDTO
+    {
+        public int ServiceID { get; set; }
+        public int CompanyID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+    }
+}
